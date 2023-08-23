9eb721b3ef9da5464dfc913c38119f9d202e500f
Greatman Ugorji <coderyker@gmail.com>
2023-08-23 12:57:51 +0100

    accordion

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const accordion = document.getElementsByClassName('container');
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
